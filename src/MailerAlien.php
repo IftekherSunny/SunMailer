72a7f80f8941bd26b038163eec3d751c61478c8d
iftekher sunny <iftekhersunny@hotmail.com>
2015-07-13 10:09:53 +0600

    Added alien support

---
+++ src/MailerAlien.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace SunMailer;
+
+class MailerAlien extends \Sun\Alien
+{
+    /**
+     * To register Alien
+     *
+     * @return object
+     */
+    public static function registerAlien()
+    {
+        return new Mailer();
+    }
+}
---
+++ src/ViewAlien.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace SunMailer;
+
+class ViewAlien extends \Sun\Alien
+{
+    /**
+     * To register Alien
+     *
+     * @return object
+     */
+    public static function registerAlien()
+    {
+        return new View();
+    }
+}
